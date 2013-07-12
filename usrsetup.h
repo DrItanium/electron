8b070d0231dc064f04026905ffd31542988a2983
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-07-11 22:26:39 -0700

    Updated the licenses of several files to make sure that they are accurate

--- usrsetup.h
+++ usrsetup.h
@@ -1,5 +1,5 @@
 /*
-The Adventure Engine
+electron
 Copyright (c) 2013, Joshua Scoggins 
 All rights reserved.
 
@@ -10,7 +10,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of Joshua Scoggins nor the
+    * Neither the name of electron nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
