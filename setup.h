25dcb275bf8cddbef2b55b1f3c3baea46fc89e8e
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-07-13 16:33:35 -0700

    Added ushort to the list of typedefs in setup.h

--- setup.h
+++ setup.h
@@ -154,6 +154,7 @@
 #else
 typedef signed char schar;
 typedef unsigned char uchar;
+typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned long ulong;
 typedef unsigned long long uvlong;
